3cae04c047fc16094f92e8f403013b82238cdf25
fakename <fakemail@gmail.com>
2023-01-15 16:03:00 +0800

    h

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include "./conio.h"
-
+#include <cstdlib>
+#include <string.h>
 int main() {
     char buffer[100];
 
     for(int i = 0; i < sizeof(buffer); i++ ) {
         buffer[i] = getch();
 
+        // backspace
+        if(buffer[i] == 127) {
+            system("clear");
+
+            buffer[i - 2] = 0;
+            i -= 2;
+
+            for(int j = 0; j < strlen(buffer); j++) {
+                printf("*");
+            }
+        }
         // if the input was enter
-        if(buffer[i] == 10) {
+        else if(buffer[i] == 10) {
             buffer[i] = 0;                  // delete the white space
 
             i = sizeof(buffer);
+        }
+        else {
+            printf("*");
         }
     }
 
